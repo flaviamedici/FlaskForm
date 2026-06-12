5e592a227fa715bc159c180ad02a7c64c3f05e30
flaviamedici <flaviaomedici@gmail.com>
2026-06-12 15:09:07 -0700

    Show submission notification

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -27,6 +27,10 @@ def index():
 
         form = Form(first_name=first_name, last_name=last_name, email=email,
                     date=date_obj, occupation=occupation)
+
+        db.session.add(form)
+        db.session.commit()
+        flash("Form submitted successfully", "success")
 
     return render_template('index.html')
 
